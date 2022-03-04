ede9175460079aa6ac32c12a65ceb36c1e243fd4
winniesi <si.good@gmail.com>
2022-03-04 16:09:43 +0800

    修改bug

--- ewechat/receive.py
+++ ewechat/receive.py
@@ -2,7 +2,7 @@ from .login import Login
 
 
 class Receive(Login):
-    def get_msg_img(self, msgId: str, content: str, type: int = 1):
+    def get_msg_img(self, msgId: str, content: str, type: int = 0):
         """下载图片"""
         return super()._base_request(
             "getMsgImg", msgId=msgId, content=content, type=type
